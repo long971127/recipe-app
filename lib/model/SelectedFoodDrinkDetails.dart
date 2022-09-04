221bccb27957dae3762208d920c0f0b0bf941d2c
Stephen Tang <long971127@gmail.com>
2022-09-05 00:57:18 +0800

    add in new model

---
+++ lib/model/SelectedFoodDrinkDetails.dart
@@ -0,0 +1,23 @@
+class SelectedFoodDrinkDetails {
+  final String name,
+      imageURL,
+      ingredient1,
+      ingredient2,
+      ingredient3,
+      ingredientMeasure1,
+      ingredientMeasure2,
+      ingredientMeasure3,
+      instruction;
+
+  SelectedFoodDrinkDetails(
+    this.name,
+    this.imageURL,
+    this.ingredient1,
+    this.ingredient2,
+    this.ingredient3,
+    this.ingredientMeasure1,
+    this.ingredientMeasure2,
+    this.ingredientMeasure3,
+    this.instruction,
+  );
+}
